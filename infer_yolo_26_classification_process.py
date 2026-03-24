e66ead6ab66491b535c2b9cd8a08688f6c618b8c
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2026-03-24 12:04:45 +0100

    chore: Qt 6

--- infer_yolo_26_classification_process.py
+++ infer_yolo_26_classification_process.py
@@ -139,7 +139,7 @@ class InferYolo26ClassificationFactory(dataprocess.CTaskFactory):
         self.info.short_description = "Inference with YOLO26 image classification models"
         self.info.path = "Plugins/Python/Classification"
         self.info.version = "1.0.0"
-        self.info.min_ikomia_version = "0.15.0"
+        self.info.min_ikomia_version = "0.16.0"
         self.info.icon_path = "images/icon.png"
         self.info.authors = "Jocher, G., Chaurasia, A., & Qiu, J"
         self.info.article = "YOLO by Ultralytics"
--- infer_yolo_26_classification_widget.py
+++ infer_yolo_26_classification_widget.py
@@ -1,9 +1,11 @@
+from PyQt6.QtWidgets import *
+
+from torch.cuda import is_available
+
 from ikomia import core, dataprocess
 from ikomia.utils import pyqtutils, qtconversion
-from infer_yolo_26_classification.infer_yolo_26_classification_process import InferYolo26ClassificationParam
 
-from PyQt5.QtWidgets import *
-from torch.cuda import is_available
+from infer_yolo_26_classification.infer_yolo_26_classification_process import InferYolo26ClassificationParam
 
 
 class InferYolo26ClassificationWidget(core.CWorkflowTaskWidget):
@@ -17,8 +19,7 @@ class InferYolo26ClassificationWidget(core.CWorkflowTaskWidget):
 
         self.grid_layout = QGridLayout()
 
-        self.check_cuda = pyqtutils.append_check(
-            self.grid_layout, "Cuda", self.parameters.cuda and is_available())
+        self.check_cuda = pyqtutils.append_check(self.grid_layout, "Cuda", self.parameters.cuda and is_available())
         self.check_cuda.setEnabled(is_available())
 
         self.combo_model = pyqtutils.append_combo(self.grid_layout, "Model name")
@@ -39,7 +40,7 @@ class InferYolo26ClassificationWidget(core.CWorkflowTaskWidget):
         self.browse_weight_file = pyqtutils.BrowseFileWidget(
             path=self.parameters.model_weight_file,
             tooltip="Select file",
-            mode=QFileDialog.ExistingFile
+            mode=QFileDialog.FileMode.ExistingFile
         )
         row = self.grid_layout.rowCount()
         self.grid_layout.addWidget(self.label_hyp, row, 0)
